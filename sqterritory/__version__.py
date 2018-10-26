53bcf7c8946f5d216d1ceccf55f9f339125b8205
Andy Eschbacher <andyesch@protonmail.com>
2018-10-26 15:24:07 -0400

    bumps version

--- sqterritory/__version__.py
+++ sqterritory/__version__.py
@@ -1,7 +1,7 @@
 """sqterritory metadata"""
 __title__ = 'sqterritory'
 __description__ = 'Prototype package for calcuating balanced territories'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __url__ = 'https://github.com/andy-esch/sqterritory'
 __author__ = 'Andy Eschbacher'
 __email__ = 'andy@carto.com'
